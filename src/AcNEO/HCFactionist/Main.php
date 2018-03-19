6f58391617260229c7135290404cc1960bf07013
AcNEO <37327658+AcNEO@users.noreply.github.com>
2018-03-19 12:33:20 +0800

    still in-complete

--- src/AcNEO/HCFactionist/Main.php
+++ src/AcNEO/HCFactionist/Main.php
@@ -1,1 +1,32 @@
+<?php
+namespace AcNEO\HCFacionist;
 
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\Server;
+use pocketmine\Player;
+
+class Main extends PluginBase implements Listener {
+
+    const LEADER = "Leader";
+    const OFFICER = "Officer";
+    const MEMBER = "Member";
+
+    const ERROR = -1;
+    
+    public function onEnable() {
+        // Code to execute when the plugin got enabled
+        // this->getServer()->getPluginManager()->registerEvents(this,this)
+    }
+    
+    public function onLoad() {
+        // Code to execute when the plugin got loaded
+    }
+    
+    public function onDisable() {
+        // Cde to execute just before the plugin gets disable
+    }
+
+
+}
+?>
