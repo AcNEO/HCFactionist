42870722d83fa804122d20380327b810ed08f1a9
AcNEO <37327658+AcNEO@users.noreply.github.com>
2018-03-19 13:25:23 +0800

    still in-complete

--- src/AcNEO/HCFactionist/EventListener/EventListener.php
+++ src/AcNEO/HCFactionist/EventListener/EventListener.php
@@ -1,1 +1,30 @@
+<?php
+namespace AcNEO\HCFactionist\EventListener;
 
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\command\{Command, CommandSender};
+use pocketmine\utils\{TextFormat, Config};
+use pocketmine\item\Item;
+use AcNEO\HCFactionist\Main;
+use AcNEO\HCFactionist\Utils\DailyQuest\DailyQuest;
+
+class EventListener implements Listener {
+
+    public function __construct(Main $main) {
+        $this->main = $main;
+    }
+
+	public function onJoinDailyQuest(PlayerJoinEvent $ev) {
+		$name = $ev->getPlayer()->getName();
+		if(DailyQuest::isFirstJoin($name)) {
+			DailyQuest::registerConfig($name);
+		}
+	}
+
+}
+?>
